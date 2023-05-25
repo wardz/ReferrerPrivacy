4e518df8cd62c2339ad11e6e60b6d4029730e049
wardz <10900212+wardz@users.noreply.github.com>
2023-05-25 08:58:53 +0200

    minor cleanup

--- content.js
+++ content.js
@@ -1,32 +1,32 @@
-// TODO: fix iframe window bypass
+// TODO: fix iframe window bypass:
 // window[0].Reflect.getOwnPropertyDescriptor(window[0].Document.prototype, "referrer").get.call(document);
+// Although might not be possible to fix synchronously without doing something stupid like hooking every dom insert node call.
+// Probably better off blocking the URL for the js script if you see some analytics lib abusing this.
 
 try {
     if (new URL(document.referrer).origin !== window.location.origin) {
-        // Set 'document.referrer' in current window to empty string
-        Reflect.defineProperty(Document.prototype, "referrer", {
-            ...Reflect.getOwnPropertyDescriptor(Document.prototype, "referrer"),
-            get: () => "",
-        });
 
-        // Set 'document.referrer' in iframe & object windows to empty string
+        function overrideReferrer(target) {
+            try {
+                Reflect.defineProperty(target, "referrer", {
+                    ...Reflect.getOwnPropertyDescriptor(target, "referrer"),
+                    get: () => "",
+                });
+            } catch {}; // incase CORS iframe error, saves a lot of URL checking
+        }
+
+        // Set 'document.referrer' in current window to empty string
+        overrideReferrer(Document.prototype);
+
+        // Set 'document.referrer' in iframe, frame & object contentWindow to empty string
         [HTMLIFrameElement, HTMLObjectElement, HTMLFrameElement].forEach((element) => {
             const windowPropDescriptor = Reflect.getOwnPropertyDescriptor(element.prototype, "contentWindow");
 
             Reflect.defineProperty(element.prototype, "contentWindow", {
                 ...windowPropDescriptor,
-
                 get: function() {
                     const contentWindow = windowPropDescriptor.get.call(this);
-
-                    if (contentWindow) {
-                        try {
-                            Reflect.defineProperty(contentWindow.Document.prototype, "referrer", {
-                                ...Reflect.getOwnPropertyDescriptor(contentWindow.Document.prototype, "referrer"),
-                                get: () => "",
-                            });
-                        } catch {};
-                    }
+                    overrideReferrer(contentWindow.Document.prototype);
 
                     return contentWindow;
                 }
@@ -35,19 +35,12 @@ try {
 
         // set 'document.referrer' in window.open to empty string
         window.open = new Proxy(window.open, {
-            apply: function(target, thisArg, argumentsList) {
+            apply: (target, thisArg, argumentsList) => {
                 const contentWindow = target.apply(thisArg, argumentsList);
-                if (contentWindow) {
-                    try {
-                        Reflect.defineProperty(contentWindow.Document.prototype, "referrer", {
-                            ...Reflect.getOwnPropertyDescriptor(contentWindow.Document.prototype, "referrer"),
-                            get: () => "",
-                        });
-                    } catch {};
-                }
+                overrideReferrer(contentWindow.Document.prototype)
 
                 return contentWindow;
-              }
+            }
         });
     }
 } catch {}
