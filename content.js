645c8c2e9dacdead7e0a733faad4ef3acbce70cb
wardz <10900212+wardz@users.noreply.github.com>
2023-05-26 02:05:55 +0200

    CRLF to LF, minor cleanup

--- content.js
+++ content.js
@@ -5,42 +5,41 @@
 
 try {
     if (new URL(document.referrer).origin !== window.location.origin) {
-
-        function overrideReferrer(target) {
+        const overrideReferrer = (target) => {
             try {
-                Reflect.defineProperty(target, "referrer", {
-                    ...Reflect.getOwnPropertyDescriptor(target, "referrer"),
-                    get: () => "",
+                Reflect.defineProperty(target, 'referrer', {
+                    ...Reflect.getOwnPropertyDescriptor(target, 'referrer'),
+                    get: () => '',
                 });
-            } catch {}; // incase CORS iframe error, saves a lot of URL checking
-        }
+            } catch {} // incase CORS iframe error, saves a lot of URL checking
+        };
 
         // Set 'document.referrer' in current window to empty string
         overrideReferrer(Document.prototype);
 
         // Set 'document.referrer' in iframe, frame & object contentWindow to empty string
         [HTMLIFrameElement, HTMLObjectElement, HTMLFrameElement].forEach((element) => {
-            const windowPropDescriptor = Reflect.getOwnPropertyDescriptor(element.prototype, "contentWindow");
+            const windowPropDescriptor = Reflect.getOwnPropertyDescriptor(element.prototype, 'contentWindow');
 
-            Reflect.defineProperty(element.prototype, "contentWindow", {
+            Reflect.defineProperty(element.prototype, 'contentWindow', {
                 ...windowPropDescriptor,
-                get: function() {
+                get() {
                     const contentWindow = windowPropDescriptor.get.call(this);
                     overrideReferrer(contentWindow.Document.prototype);
 
                     return contentWindow;
-                }
+                },
             });
         });
 
         // set 'document.referrer' in window.open to empty string
         window.open = new Proxy(window.open, {
-            apply: (target, thisArg, argumentsList) => {
+            apply(target, thisArg, argumentsList) {
                 const contentWindow = target.apply(thisArg, argumentsList);
-                overrideReferrer(contentWindow.Document.prototype)
+                overrideReferrer(contentWindow.Document.prototype);
 
                 return contentWindow;
-            }
+            },
         });
     }
 } catch {}
